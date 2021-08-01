1393fedb07c4ffd3a1093076018303e546b6241e
JiYoung <75613475+dlwldud1151@users.noreply.github.com>
2021-08-01 15:56:56 +0900

    ES6 문자열 문법 익히기

---
+++ step08/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ES6_1 from './ES6_1';
+
+function App() {
+  return (
+    <div>
+      <h1>ES6 문법 연습</h1>
+      <ES6_1/>
+    </div>
+  )
+}
+
+export default App;
