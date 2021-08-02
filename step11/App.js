e0bf267f7031ae46b10e81e141d9c54248a953ef
JiYoung <75613475+dlwldud1151@users.noreply.github.com>
2021-08-02 20:49:37 +0900

    reactstrap Alerts 사용하기

---
+++ step11/App.js
@@ -0,0 +1,17 @@
+/*esline-disable*/
+import React from 'react';
+import './App.css';
+import ReactStrapAlerts from './ReactStrapAlerts';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div>
+      <h1>reactstrap</h1>
+      <p>Alerts 패키지 사용</p>
+      <ReactStrapAlerts/>
+    </div>
+  )
+}
+
+export default App;
---
+++ step11/ReactStrapAlerts.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Alert, UncontrolledAlert} from 'reactstrap';
+
+class ReactStrapAlerts extends Component{
+    render(){
+        return(
+            <div>
+                <Alert color="light">
+                    Simple Alert [color : light]
+                </Alert>
+                <UncontrolledAlert color="warning">
+                    UncontrolledAlert Alert [color: warning]
+                </UncontrolledAlert>
+            </div>
+        )
+    }
+}
+
+export default ReactStrapAlerts
